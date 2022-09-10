4bab4d2cffe85b10118f4f5adac297de7d1bdec2
BHC-Chicken <tij05078@gmail.com>
2022-09-10 22:21:22 +0900

    user repo 추가

---
+++ src/main/java/dev/ioexception/unnamed/repository/UserAccountRepository.java
@@ -0,0 +1,7 @@
+package dev.ioexception.unnamed.repository;
+
+import dev.ioexception.unnamed.domain.UserAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserAccountRepository extends JpaRepository<UserAccount, String> {
+}
