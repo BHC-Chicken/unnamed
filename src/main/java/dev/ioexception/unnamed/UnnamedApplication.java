cb913401579a19d16ac86d3e51c77dedf98afec2
BHC-Chicken <tij05078@gmail.com>
2022-09-10 22:21:36 +0900

    etc

--- src/main/java/dev/ioexception/unnamed/UnnamedApplication.java
+++ src/main/java/dev/ioexception/unnamed/UnnamedApplication.java
@@ -2,7 +2,9 @@ package dev.ioexception.unnamed;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class UnnamedApplication {
 
